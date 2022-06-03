88b72859406df23e093198292fee0ee6b21a669a
stacc <stacc@staccs-MacBook-Air.local>
2022-06-03 00:26:38 -0300

    Updates

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const SOLANA_URL =
-  process.env.NEXT_PUBLIC_SOLANA_URL ||
-  "https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/";
-
-export const GA_TRACKING_ID =
-  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "G-L4QLBX3394";
-
-export const IS_PRODUCTION = process.env.NODE_ENV === "production";
-export const GIPHY_API_KEY = process.env.NEXT_PUBLIC_GIPHY_API_KEY!
--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./useChat";
-export * from "./useChatKey";
-export * from "./useProfileKey";
-export * from "./useWalletProfile";
-export * from "./useProfile";
-export * from "./useMessages";
-export * from "./useDelegateWallet";
